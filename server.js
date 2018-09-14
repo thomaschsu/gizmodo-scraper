2f4e0143f47759ebff046ddae4d51222dce04548
Tom Hsu <thomaschsu@gmail.com>
2018-09-14 15:34:47 -0400

    Changed to techspot and changed to H2

--- server.js
+++ server.js
@@ -36,19 +36,19 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 // Connect to the Mongo DB
-mongoose.connect("mongodb://localhost/week18Populater", { useNewUrlParser: true });
+mongoose.connect("mongodb://localhost/newsscrape", { useNewUrlParser: true });
 
 // Routes
 
 // A GET route for scraping the echoJS website
 app.get("/scrape", function(req, res) {
   // First, we grab the body of the html with request
-  axios.get("http://www.echojs.com/").then(function(response) {
+  axios.get("http://www.techspot.com/").then(function(response) {
     // Then, we load that into cheerio and save it to $ for a shorthand selector
     var $ = cheerio.load(response.data);
 
-    // Now, we grab every h2 within an article tag, and do the following:
-    $("article h2").each(function(i, element) {
+    // Now, we grab every h2:
+    $("h2").each(function(i, element) {
       // Save an empty result object
       var result = {};
 
