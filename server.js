7c8a264099aee3a409cff99f56ff49c945c4d59b
Tom Hsu <thomaschsu@gmail.com>
2018-09-14 00:44:06 -0400

    Added handlebars

--- server.js
+++ server.js
@@ -4,20 +4,24 @@ var logger = require("morgan");
 var mongoose = require("mongoose");
 
 // Our scraping tools
-// Axios is a promised-based http library, similar to jQuery's Ajax method
-// It works on the client and on the server
 var axios = require("axios");
 var cheerio = require("cheerio");
 
 // Require all models
 var db = require("./models");
-
 var PORT = 3000;
 
 // Initialize Express
 var app = express();
 
 // Configure middleware
+
+// Set Handlebars.
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
 
 // Use morgan logger for logging requests
 app.use(logger("dev"));
