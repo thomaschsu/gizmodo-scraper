b5d80d9e6b0521b42ae088bdb7839a21ed027a09
Tom Hsu <thomaschsu@gmail.com>
2018-09-15 16:11:37 -0400

    Added clear articles button

--- server.js
+++ server.js
@@ -77,6 +77,7 @@ app.get("/scrape", function(req, res) {
     // If we were able to successfully scrape and save an Article, send a message to the client
     res.send("Scrape Complete");
   });
+  res.render('index');
 });
 
 // Route for getting all Articles from the db
