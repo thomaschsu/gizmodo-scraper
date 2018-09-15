3351be49e77551e1a92a07b6e3df205d39d1bb54
Tom Hsu <thomaschsu@gmail.com>
2018-09-14 22:05:36 -0400

    Changed to gizmodo

--- server.js
+++ server.js
@@ -43,7 +43,7 @@ mongoose.connect("mongodb://localhost/newsscrape", { useNewUrlParser: true });
 // A GET route for scraping the echoJS website
 app.get("/scrape", function(req, res) {
   // First, we grab the body of the html with request
-  axios.get("http://www.techspot.com/").then(function(response) {
+  axios.get("http://www.gizmodo.com/").then(function(response) {
     // Then, we load that into cheerio and save it to $ for a shorthand selector
     var $ = cheerio.load(response.data);
 
