ca94eba4f2115b6a7fe3c168dae93c88b4f489fb
Tom Hsu <thomaschsu@gmail.com>
2018-10-05 14:40:47 -0400

    Modified server

--- server.js
+++ server.js
@@ -50,13 +50,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // Use express.static to serve the public folder as a static directory
 app.use(express.static("public"));
 
-// If deployed, use the deployed database. Otherwise use the local newsscrape database
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/newsscrape";
-
-// Set mongoose to leverage built in JavaScript ES6 Promises
 // Connect to the Mongo DB
-mongoose.Promise = Promise;
-mongoose.connect(MONGODB_URI);
+mongoose.connect("mongodb://localhost/newsscrape", { useNewUrlParser: true });
 
 // Routes
 
